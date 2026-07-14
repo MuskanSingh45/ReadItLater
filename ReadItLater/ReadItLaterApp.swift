f1596e1d7e5fab6fad840dfe78c01502896b87a8
Aman Singh <aman.s19452@gmail.com>
2026-07-14 23:38:40 +0530

    Initial Commit

---
+++ ReadItLater/ReadItLaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadItLaterApp.swift
+//  ReadItLater
+//
+//  Created by Mac on 14/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ReadItLaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
